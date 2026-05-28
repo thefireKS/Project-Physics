ca44cac3509424d46aa94c1fc9509b4dcc957f01
thefireKS <KS29092003@gmail.com>
2026-05-28 18:22:48 +0300

    Most final fixes ever. New build.

--- Assets/Scripts/jija.cs
+++ Assets/Scripts/jija.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class jija : MonoBehaviour {
 	public Animator anim;
 	int i = 0;
+	bool isPlaying = false;
 	void Start () {
 		anim = GetComponent <Animator> ();
 	}
@@ -16,9 +17,16 @@ public class jija : MonoBehaviour {
 	}
 	public void Update (){
 		if (OofCount.qwerty == true && TimeCount.timer > 2.4f) {
-			anim.Play ("Water");
+			if (!isPlaying) {
+				i = 0;
+				anim.speed = 1;
+				anim.Play ("Water", 0, 0f);
+				isPlaying = true;
+			}
 			if (i==1)
 				anim.speed = 0;
+		} else {
+			isPlaying = false;
 		}
 	}
 }
